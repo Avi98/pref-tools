859dab7abebe06d9fceeef0a8a40c8259066db78
Avinash <avinash.kumar@renegadeinsurance.com>
2022-10-18 13:31:42 +0530

    chore: test setup.

--- packages/cli/jest.config.ts
+++ packages/cli/jest.config.ts
@@ -11,6 +11,6 @@ export default {
   transform: {
     '^.+\\.[tj]sx?$': 'ts-jest',
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['ts', 'js'],
   coverageDirectory: '../../coverage/packages/cli',
 };
